5b193c56e8a10ddaef530b42d8840720dba7c996
rina2004 <shinnolove.2004@gmail.com>
2024-10-22 15:04:45 +0700

    Add access control

---
+++ PRJ301_Assignment/src/java/controller_accesscontrol/LoginController.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller_accesscontrol;
+
+import dal.UserDBContext;
+import model_accesscontrol.User;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Rinaaaa
+ */
+public class LoginController extends HttpServlet {
+   
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+        //
+        request.getRequestDispatcher("login.html").forward(request, response);
+        //
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+        UserDBContext db = new UserDBContext();
+        User account = db.get(user, pass);
+        
+        if(account!=null)
+        {
+            request.getSession().setAttribute("account", account);
+            response.getWriter().println("login successful!");
+        }
+        else
+            response.getWriter().println("login failed!");
+        
+        response.getWriter().println(getInitParameter("private"));
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ PRJ301_Assignment/src/java/dal/UserDBContext.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.util.ArrayList;
+import model_accesscontrol.User;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model_accesscontrol.Feature;
+import model_accesscontrol.Role;
+
+/**
+ *
+ * @author Rinaaaa
+ */
+public class UserDBContext extends DBContext<User> {
+
+    public ArrayList<Role> getRoles(String username) {
+        ArrayList<Role> roles = new ArrayList<>();
+        String sql = "SELECT r.rid,r.rname,f.fid,f.fname,f.url FROM [User] u \n"
+                + "	INNER JOIN UserRole ur ON ur.username = u.username\n"
+                + "	INNER JOIN [Role] r ON r.rid = ur.rid\n"
+                + "	INNER JOIN RoleFeature rf ON rf.rid = r.rid\n"
+                + "	INNER JOIN Feature f ON f.fid = rf.fid\n"
+                + "WHERE u.username = ?\n"
+                + "ORDER BY r.rid ASC, f.fid ASC";
+        
+        PreparedStatement stm = null;
+        try {
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, username);
+            ResultSet rs = stm.executeQuery();
+            Role crole = new Role();
+            crole.setId(-1);
+            while(rs.next())
+            {
+                int rid = rs.getInt("rid");
+                if(rid!= crole.getId())
+                {
+                    crole = new Role();
+                    crole.setId(rid);
+                    crole.setName(rs.getString("rname"));
+                    roles.add(crole);
+                }
+                Feature f = new Feature();
+                f.setId(rs.getInt("fid"));
+                f.setName(rs.getString("fname"));
+                f.setUrl(rs.getString("url"));
+                crole.getFeatures().add(f);
+                f.setRoles(roles);
+                
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally
+        {
+            try {
+                stm.close();
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return roles;
+    }
+
+    public User get(String username, String password) {
+        String sql = "SELECT [username]\n"
+                + "    ,[password]\n"
+                + "    ,[displayname]\n"
+                + "FROM [User]\n"
+                + "WHERE username = ? AND [password] = ?";
+
+        PreparedStatement stm = null;
+        User user = null;
+        try {
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, username);
+            stm.setString(2, password);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                user = new User();
+                user.setDisplayname(rs.getString("displayname"));
+                user.setUsername(username);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                stm.close();
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return user;
+    }
+
+    @Override
+    public void insert(User model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(User model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(User model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<User> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public User get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
